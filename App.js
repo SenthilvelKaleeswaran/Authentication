e47988ee8cc441a6579c604b5f683877ed9089ec
SENTHILVEL <senthilvel@SENTHILVELs-MacBook-Air.local>
2023-06-14 12:27:33 +0530

    Navigation and api implementation

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import React from 'react'
-
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { SafeAreaView, Text, View } from 'react-native'
 import Login from './src/screens/Login'
 import Signup from './src/screens/Signup'
+import Profile from './src/screens/Profile/Profile'
+
+
+const Stack = createNativeStackNavigator()
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <Signup/>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login' >
+        <Stack.Screen name='Login' component={Login}  options={{headerShown:false}} />
+        <Stack.Screen name='Signup' component={Signup}  options={{headerShown:false}} />
+        <Stack.Screen name='Profile' component={Profile}  options={{headerShown:false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
+   
   )
 }
 
--- src/screens/Signup/index.js
+++ src/screens/Signup/index.js
@@ -1,167 +1,198 @@
-import React, { useState } from 'react';
-import { Alert, Button, Modal, ScrollView, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import {
+  Button,
+  Modal,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native'
 // import CheckBox from '@react-native-community';
-import CheckBox from '@react-native-community/checkbox';
+import CheckBox from '@react-native-community/checkbox'
 
-import AuthButton from '../../components/Auth/Button/AuthButton';
-import { PasswordFormField, TextFormField } from '../../components/Auth/FormField/FormField';
+import AuthButton from '../../components/Auth/Button/AuthButton'
+import {
+  PasswordField,
+  TextField,
+} from '../../components/Auth/FormField/FormField'
 
-const Signup = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [check, setCheck] = useState(false);
-  const [openModal,setOpenModal] = useState(false)
+const Signup = ({ navigation }) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [check, setCheck] = useState(false)
+  const [openModal, setOpenModal] = useState(false)
 
   const handleSignupButtonSubmit = () => {
     setOpenModal(true)
-  };
+  }
 
   const handleCheckBoxChange = () => {
-    setCheck(!check);
-  };
+    setCheck(!check)
+  }
 
-  const handleModalOpen =() =>{
-
+  const handleModalOpen = () => {
     setOpenModal(false)
-
+  }
+  const handleLoginClick = () => {
+    navigation.navigate('Login')
   }
 
   return (
-    <View style={styles.container}>  
-       <ScrollView>
-      <View style={styles.containerHeader}> 
-        <Text style={styles.containerHeaderText}>Sign Up</Text>
-        <Text style={[styles.text,styles.containerHeaderNote]}>Enter your details below & Sign up</Text>
-      </View>
-
-      <View style={styles.containerBody}>
-        <TextFormField label="Your Email" placeHolder="email" value={email} onChangeText={setEmail} />
-        <PasswordFormField label="Password" placeHolder="password" value={password} onChangeText={setPassword} />
-        <AuthButton text="Create account" handleOnPress={handleSignupButtonSubmit} />
- 
-        <View style={styles.checkBoxContainer}>
-          <CheckBox
-            value={check}
-            onValueChange={handleCheckBoxChange} 
-
-          />
-          <Text style={[styles.text,styles.checkBoxText]}>
-            By creating an account you have to agree with our terms and conditions
+    <View style={styles.container}>
+      <ScrollView>
+        <View style={styles.containerHeader}>
+          <Text style={styles.containerHeaderText}>Sign Up</Text>
+          <Text style={[styles.text, styles.containerHeaderNote]}>
+            Enter your details below & Sign up
           </Text>
-        </View><Text style={[styles.text,styles.textAlready]}>Already have an account? Log in</Text>
-      
-      </View>
-
-      </ScrollView>
-      
-
-        
-      <Modal
-        animationType='slide'
-        transparent={true}
-        visible={openModal}
-        onRequestClose={handleModalOpen}
-      >
-        <View style={styles.modalContainer}>
-          <View style={styles.modalInnerContainer}>
-            <Text>Sucessfully account created</Text>
-            <Button title='Done' onPress={handleModalOpen}/>
-          </View>
         </View>
 
+        <View style={styles.containerBody}>
+          <TextField
+            label="Your Email"
+            placeHolder="email"
+            value={email}
+            onChangeText={setEmail}
+          />
+          <PasswordField
+            label="Password"
+            placeHolder="password"
+            value={password}
+            onChangeText={setPassword}
+          />
+          <AuthButton
+            text="Create account"
+            handleOnPress={handleSignupButtonSubmit}
+          />
+
+          <View style={styles.checkBoxContainer}>
+            <CheckBox value={check} onValueChange={handleCheckBoxChange} />
+            <Text style={[styles.text, styles.checkBoxText]}>
+              By creating an account you have to agree with our terms and
+              conditions
+            </Text>
+          </View>
+          <View style={styles.loginNavigator}>
+            <Text style={[styles.text, styles.textAlready]}>
+              Already have an account?
+            </Text>
+            <TouchableOpacity
+              style={styles.loginNavigatonTextContainer}
+              onPress={handleLoginClick}>
+              <Text style={[styles.loginNavigatorText, styles.text]}>
+                Login
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={openModal}
+        onRequestClose={handleModalOpen}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalInnerContainer}>
+            <Text style={styles.moddalText}>Sucessfully account created</Text>
+            <AuthButton text="Done" handleOnPress={handleModalOpen} />
+          </View>
+        </View>
       </Modal>
-
-
-
     </View>
-  );
-};
+  )
+}
 
-const styles= StyleSheet.create({
-  container : {
-
-    flexDirection:'column',
-    backgroundColor:'#F0F0F2',
-
+const styles = StyleSheet.create({
+  loginNavigator: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 
-  containerHeader : {
- 
+  loginNavigatorText: {
+    color: '#3D5CFF',
+  },
+
+  container: {
     flexDirection: 'column',
-    marginTop:70,
-    marginBottom:10,
-    
+    backgroundColor: '#F0F0F2',
   },
 
-  containerHeaderText : {
-
-    fontFamily:'poppins',
-    color:'#1F1F39',
-    fontSize:32,
-    lineHeight:48,
-    fontWeight:'800', 
-    paddingLeft:24,
-    
-  },
-  
-  text : {
-    fontFamily:'Poppins',
-    fontSize:16,
-    fontWeight:'normal',       
-    letterSpacing:0.5,
-    lineHeight:21,
-   
+  containerHeader: {
+    flexDirection: 'column',
+    marginTop: 70,
+    marginBottom: 10,
   },
 
-  containerHeaderNote : {
-    paddingLeft:26,
-    color:'#B8B8D2',
-    textAlign:'left',
-    
+  containerHeaderText: {
+    fontFamily: 'poppins',
+    color: '#1F1F39',
+    fontSize: 32,
+    lineHeight: 48,
+    fontWeight: '800',
+    paddingLeft: 24,
   },
 
-  textAlready:{
+  text: {
+    fontFamily: 'Poppins',
+    fontSize: 16,
+    fontWeight: 'normal',
+    letterSpacing: 0.5,
+    lineHeight: 21,
+  },
 
-    textAlign:'center',
-    color:'#858597',
-    padding:40,
+  moddalText: {
+    fontFamily: 'Poppins',
+    fontSize: 16,
+    fontWeight: 'normal',
+    letterSpacing: 0.5,
+    lineHeight: 21,
+  },
+
+  containerHeaderNote: {
+    paddingLeft: 26,
+    color: '#B8B8D2',
+    textAlign: 'left',
+  },
+
+  textAlready: {
+    textAlign: 'right',
+    color: '#858597',
+    padding: 40,
   },
 
   containerBody: {
-
-    borderTopLeftRadius:10,
-    borderTopRightRadius:10,
-    backgroundColor:'white',
-    padding:16,
-    paddingTop:25,
-
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    backgroundColor: 'white',
+    padding: 16,
+    paddingTop: 25,
   },
 
-  checkBoxContainer:{
-    flexDirection:'row',
+  checkBoxContainer: {
+    flexDirection: 'row',
   },
 
-  checkBoxText : {
-    padding:0,
-
+  checkBoxText: {
+    padding: 0,
   },
 
-  modalContainer : {
-    backgroundColor:'grey',
-    height:'100%',
-    alignItems:'center',
-
+  modalContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    backgroundColor: 'grey',
+    height: '100%',
+    alignItems: 'center',
   },
-  modalInnerContainer:{
-
-    backgroundColor:'white',
-    height:'50%',
-    width:'90%',
-
-
-  }
-
-
+  modalInnerContainer: {
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '50%',
+    width: '90%',
+    borderRadius: 10,
+  },
 })
 
-export default Signup;
+export default Signup
